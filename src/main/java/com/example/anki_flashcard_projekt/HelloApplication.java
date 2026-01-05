b34ba372ddd36ea72a16000ef276981f08186c04
emmRas01 <emmras01@easv365.dk>
2026-01-05 11:49:17 +0100

    Small Adjustments

--- src/main/java/com/example/anki_flashcard_projekt/HelloApplication.java
+++ src/main/java/com/example/anki_flashcard_projekt/HelloApplication.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application
+{
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("flashcardSætOversigt.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Flashcards");
         stage.setScene(scene);
         stage.show();
     }
